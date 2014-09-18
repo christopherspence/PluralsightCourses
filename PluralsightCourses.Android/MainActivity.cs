f614b340992e8800e7b21d201fda7476e38f1236
Christopher Spence <ghaladen@gmail.com>
2014-09-18 17:33:31 -0500

    Added Android Project

---
+++ PluralsightCourses.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PluralsightCourses.Android
+{
+    [Activity(Label="PluralsightCourses", MainLauncher=true, Icon="@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button buttonPrev;
+        Button buttonNext;
+        TextView textTitle;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            this.buttonPrev = FindViewById<Button>(Resource.Id.buttonPrev);
+            this.buttonNext = FindViewById<Button>(Resource.Id.buttonNext);
+            this.textTitle = FindViewById<TextView>(Resource.Id.textTitle);
+
+            this.buttonPrev.Click += buttonPrev_Click;
+            this.buttonNext.Click += buttonNext_Click;
+        }
+
+        void buttonPrev_Click(object sender, EventArgs e)
+        {
+            this.textTitle.Text = "Prev Clicked";
+        }
+
+        void buttonNext_Click(object sender, EventArgs e)
+        {
+            this.textTitle.Text = "Next Clicked";
+        }
+    }
+}
+
