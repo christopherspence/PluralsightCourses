b5cb2b6b6da88f8a12a1c80be90d87c8af259d7a
Christopher Spence <ghaladen@gmail.com>
2014-09-18 17:44:15 -0500

    Added More Content to Android project

--- PluralsightCourses.Android/Resources/Resource.Designer.cs
+++ PluralsightCourses.Android/Resources/Resource.Designer.cs
@@ -47,6 +47,12 @@ namespace PluralsightCourses.Android
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int ps_top_card_01 = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int ps_top_card_02 = 2130837506;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
