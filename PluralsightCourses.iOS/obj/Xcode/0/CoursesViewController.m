d46550ed1691bcca96f5498ea5073654e629d6fb
christopherspence <cspence@orbitalsoft.com>
2014-09-19 12:02:31 -0500

    Added Courses UI

---
+++ PluralsightCourses.iOS/obj/Xcode/0/CoursesViewController.m
@@ -0,0 +1,18 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import "CoursesViewController.h"
+
+@implementation CoursesViewController
+
+- (void)dealloc {
+    [_buttonPrev release];
+    [_buttonNext release];
+    [_labelTitle release];
+    [super dealloc];
+}
+@end
