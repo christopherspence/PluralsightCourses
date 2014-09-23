77fdd0b7942e46026ad5ad1af0a7b4c800a3a195
Christopher Spence <cspence@orbitalsoft.com>
2014-09-23 01:56:45 -0500

    Started Shared Library

---
+++ Courses.Library/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Courses.Library
+{
+    public class Course
+    {
+        public string Title { get; internal set; }
+        public string Description { get; internal set; }
+        public string Image { get; internal set; }
+    }
+}
